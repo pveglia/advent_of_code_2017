7d01dff93b36921945ab8367217342d7aab7231e
Paolo Veglia <pveglia@gmail.com>
2017-12-06 23:02:05 +0100

    refactoring

--- day6.py
+++ day6.py
@@ -2,23 +2,14 @@ def load_file():
     with open('./day6.input.txt') as f:
         return [int(b) for b in f.read().split('\t')]
 
-def getMaxBucket(buckets):
-    max = 0
-    index = 0
-    for idx, b in enumerate(buckets):
-        if b > max:
-            max = b
-            index = idx
-    return (index, max)
-
 def part1():
     fingerprints = {}
     buckets = load_file()
-    print(buckets)
     steps = 0
     fingerprints[tuple(buckets)] = 0
     while True:
-        bmaxBucket, blocks = getMaxBucket(buckets) 
+        bmaxBucket, blocks = max(enumerate(buckets), key=lambda i: i[1])
+
         buckets[bmaxBucket] = 0
         increment = blocks // len(buckets)
         for idx, _ in enumerate(buckets):
@@ -26,13 +17,11 @@ def part1():
         for idx in range(blocks % len(buckets)):
             buckets[(bmaxBucket + 1 + idx) % len(buckets)] += 1
         steps += 1
-        print('after step {}'.format(steps))
-        print(buckets)
         if tuple(buckets) in fingerprints:
-            print('found after {} steps'.format(steps))
-            print('cycle size {}'.format(steps - fingerprints[tuple(buckets)]))
             break
         fingerprints[tuple(buckets)] = steps
+    print('found after {} steps'.format(steps))
+    print('cycle size {}'.format(steps - fingerprints[tuple(buckets)]))
 
 
 if __name__ == '__main__':
