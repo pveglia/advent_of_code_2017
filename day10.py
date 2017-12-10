c14c27b0ba2d665f646386805d98a11edc08fb18
Paolo Veglia <pveglia@gmail.com>
2017-12-10 12:16:23 +0100

    day10

---
+++ day10.py
@@ -0,0 +1,27 @@
+from functools import reduce
+from itertools import zip_longest
+
+def reverse(list, start_pos, n):
+    s = start_pos
+    e = start_pos + n -1
+    while s < e:
+        list[s%ll], list[e%ll] = list[e%ll], list[s%ll]
+        s += 1
+        e += -1
+    return list
+
+def group(l, n):
+    args = [iter(l)] * n
+    return list(zip_longest(*args))
+
+lengths = [ord(i) for i in open('day10.input.txt').read()] + [17, 31, 73, 47, 23]
+l = list(range(256))
+ll = len(l)
+current = 0
+skip = 0
+for _ in range(64):
+    for i in lengths:
+        reverse(l, current, i)
+        current += (i + skip) % ll
+        skip += 1
+print("".join([format(reduce(lambda acc, i: acc ^ i, g), '02x') for g in group(l, 16)]))
